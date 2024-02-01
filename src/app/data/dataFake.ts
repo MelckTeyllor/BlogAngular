2fafa9e53fb2137c9c8988a13d453197e6f17938
Melck Teyllor <melcksedeck123@gmail.com>
2024-02-01 09:54:51 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,52 @@
+export const dataFake = [
+    {
+        "id": "0",
+        "title": "Solo leveling",
+        "description": "Animação coreana é o mais novo sucesso na temporada",
+        "photoCover": "https://a.storyblok.com/f/178900/1920x1080/7ab3bbba84/solo-leveling.png/m/1200x0/filters:quality(95)format(webp)"
+
+    },
+    {
+        "id": "1",
+        "title": "Novo Imperador do Mar",
+        "description": "Governo anuncia novo imperador do mar após acontecimmentos em Wano...",
+        "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtQeTzcnyxIv9eMAVo-wJtdXA0S0VYh3jFXTvqyoV_spXdlBU56c6iikcQDprmv1QaOPY&usqp=CAU%22https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtQeTzcnyxIv9eMAVo-wJtdXA0S0VYh3jFXTvqyoV_spXdlBU56c6iikcQDprmv1QaOPY&usqp=CAU%22"
+    },
+
+    {
+        "id": "2",
+        "title": "Nova saga de DBZ é anunciada!",
+        "description": "...",
+        "photoCover": "https://img.olhardigital.com.br/wp-content/uploads/2021/04/dragon-ball-z.png"
+
+    },
+    {
+        "id": "3",
+        "title": "Universo de hunter x hunter tem continuidade no mangá",
+        "description": "...",
+        "photoCover": "https://s2-techtudo.glbimg.com/9NlxPMZcnf0xvkNftuOKV9WrruU=/0x0:1280x720/888x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/r/K/xlKVduRIGge6Ay0CRVPQ/aaaabfou5myz-a88bawig3doljnfl9bd-jtkw9yz8eh1rm568mtk5qgjclghndr2x8jy5pzufefr3ryz55qsxuhqcvpqniaiivdsbppf.jpg"
+
+    },
+    {
+        "id": "4",
+        "title": "Acaba de sair trailer do novo arco de Demon Slayer",
+        "description": "...",
+        "photoCover": "https://image.api.playstation.com/vulcan/ap/rnd/202106/1704/JzL1NLQvok7Pghe9W5PP2XNV.png"
+
+    },
+    {
+        "id": "5",
+        "title": "Confirmadda! Nova temporada de Fire Force anunciada para",
+        "description": "...",
+        "photoCover": "https://upload.wikimedia.org/wikipedia/pt/7/76/FireForceVol1.jpg"
+
+    },
+    {
+        "id": "6",
+        "title": "Nova saga de DBZ é anunciada!",
+        "description": "...",
+        "photoCover": "https://img.olhardigital.com.br/wp-content/uploads/2021/04/dragon-ball-z.png"
+
+    }
+]
+    
